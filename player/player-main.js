bf7a89fa46269ec70ffa3f7d011461e6614b3ce4
Negend <nneji.2day@yahoo.co.uk>
2017-08-18 10:51:06 +0100

    Update player-main.js
    see if we can get play to work

---
+++ player/player-main.js
@@ -0,0 +1,15 @@
+// find audio button by id  make its click trigger play/pause
+// toggle play pause on click
+// next for next track
+// previous for previous
+ $($('.play')[0]).click(event){
+ //if audio play == true
+ $('#player').play()
+ // else $('#audio').pause()
+ }
+ 
+ // find current audio track..... $('.song-title').html = current audio
+ 
+ 
+ // find elements by class different song sources or params ;
+ // for each song $('#songlist').appendchild = "<h5 class='track track1'>" + this + "</h5>"
