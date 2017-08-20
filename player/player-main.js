b87ad1ec59c300ccf478e2df66b1c13937912760
Nneji Nwakamma <nnwakamma@spartaglobal.co>
2017-08-20 03:34:17 +0100

    java

--- player/player-main.js
+++ player/player-main.js
@@ -2,20 +2,108 @@
 // toggle play pause on click
 // next for next track
 // previous for previous
- $(function() {
-  console.log('luminous')
-  $(".play").click(function(e) {
-  //if audio play == true
-         $(".middle").css("background-color:red")
-         var song = $(this).next('audio').get(0);
-       if (song.paused)
-         song.play();
-       else
-         song.pause();
-  // else $('#audio').pause()
- })
- })
- 
+console.log('Welcome devs, to my site. Still fixing it up so go easy. DONT FORGET TO DOWLOAD MUSIC TOO :-) ')
+$(function() {
+var playing = false
+
+
+var t = 0
+var track = new Audio('song/stand1.mp3')
+var tracks = ['song/stand1.mp3','song/drunk.mp3','song/stand3.mp3']
+pausePlay()
+nextSong()
+prevSong()
+
+
+
+
+
+
+function pausePlay(){
+	
+	$(".play").click(function(e) { 
+	  if (playing == false) {
+		  playTrack()
+		} 
+		else{
+			pauseTrack()
+		}
+	})
+}
+
+function nextSong(){
+	var cont
+	$(".next").click(function(e){
+
+	// stop whatever is playing 
+		if (playing == true){		
+			pauseTrack()
+			cont = true
+
+		}
+	// on to the next source on track list
+		t = t + 1
+		if (t == tracks.length){
+			t = 0
+		}
+	// insert next track
+		track.src = tracks[t]
+
+	// if audio was already playing, start playing immediately
+		if (cont == true){
+			playTrack()
+		}
+	})
+}
+
+function prevSong(){
+	var cont
+	
+	$(".previous").click(function(e){
+		console.log(track.currentTime)
+	// stop whatever is playing 
+		if (playing == true){		
+			pauseTrack()
+			cont = true
+
+		}
+	// on to the previous source on track list or reset current
+		if (track.currentTime < 5)
+		t = t - 1
+		if (t < 0){
+			t = 0
+		}
+	// insert next track
+		track.src = tracks[t]
+
+	// if audio was already playing, start playing immediately
+		if (cont == true){
+			playTrack()
+		}
+	})
+}
+
+
+
+
+
+function playTrack(){
+	track.play()
+	playing = true
+}
+
+function pauseTrack(){
+	track.pause()
+	playing = false
+}
+
+
+
+
+
+
+})
+	 
  // find current audio track..... $('.song-title').html = current audio
  
  
